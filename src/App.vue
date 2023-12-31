f65492aa2e69a82ac4ae79938d30d563e293142f
Rodrigo Beckmann <rodrigodsbeckmann@gmail.com>
2023-12-31 16:14:03 -0300

    ci: teste

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,95 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import { fetchPosts } from './services'
+import { fetchPosts, createPost, adminLogin, verifyLogin, deletePost } from './services'
+import {ref, onMounted, onBeforeMount} from 'vue'
 
-fetchPosts()
+const posts = ref([])
+const title = ref('')
+const content = ref('')
+const logged = ref(false)
+
+
+
+const handleSubmit = async (event) => {
+  event.preventDefault()
+  await createPost({ title: title.value, content: content.value })
+  posts.value = await fetchPosts()
+}
+
+const handleLogin = async (event) => {
+  event.preventDefault()
+  const response = await adminLogin({ email: email.value, password: password.value })
+  posts.value = await fetchPosts()
+  if (response) {
+    logged.value = true
+  }
+}
+
+const logoff = async () => {
+  const pastDate = new Date(0).toUTCString();
+  document.cookie = 'token=; expires=' + pastDate + '; path=/;';
+  logged.value = false
+}
+
+const handleDelete = async (id) => {
+  await deletePost(id)
+  posts.value = posts.value.filter(post => post.id !== id)
+}
+
+onBeforeMount(async () => {
+ if (await verifyLogin()) {
+   logged.value = true
+ }
+})
+
+onMounted(async () => {
+  posts.value = await fetchPosts()
+})
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <h1>testesteste</h1>
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+
+  <form @submit="handleLogin">
+    <label for="email">email</label>
+    <input id="email" type="email">
+    <br>
+    <br>
+    <br>
+    <label for="password">password</label>
+    <input id="password" type="password">
+    <br>
+    <br>
+    <br>
+    <button :disabled="logged" type="submit">Submit</button>
+  </form>
+
+  <button :disabled="!logged" @click="logoff">logoff</button>
+
+  <br>
+  <br>
+  <br>
+
+  <form @submit="handleSubmit" >
+    <label for="title">Title</label>
+    <input v-model="title" id="title" type="text">
+    <br>
+    <br>
+    <br>
+    <label for="content">Content</label>
+    <textarea v-model="content" id="content"></textarea>
+    <br>
+    <br>
+    <br>
+    <button type="submit">Submit</button>
+  </form>
+
+<ul>
+  <li v-for="post in posts" style="list-style: none;">
+    <p>Titulo: {{ post.title }}</p>
+    <p>{{ post.content }}</p>
+    <button @click="handleDelete(post.id)">x</button>
+  </li>
+</ul>
 </template>
 
 <style scoped>
--- src/services/index.js
+++ src/services/index.js
@@ -2,7 +2,73 @@ import axios from 'axios';
 
 const API_URL = import.meta.env.VITE_API_URL;
 
+function getCookie(name) {
+  const cookies = document.cookie.split(';');
+  for (const cookie of cookies) {
+    const [cookieName, cookieValue] = cookie.trim().split('=');
+    if (cookieName === name) {
+      return decodeURIComponent(cookieValue);
+    }
+  }
+  return null; // Cookie não encontrado
+}
+
+
+
 export const fetchPosts = async () => {
-const response = await axios.get(`${API_URL}/posts`);
-console.log(response.data);
+  const response = await axios.get(`${API_URL}/posts`);
+  return response.data;
+}
+
+export const createPost = async (post) => {
+  const token = getCookie('token');
+  await axios.post(`${API_URL}/posts`, post, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+}
+
+export const adminLogin = async (credentials) => {
+  const token = await axios.post(`${API_URL}/login`, credentials);
+  document.cookie = `token=${token.data.token}`;
+  if (token.data.token) {
+    return true;
+  }
+  return false;
+}
+
+export const verifyLogin = async () => {
+  // const token = getCookie('token');
+  // const user = await axios.get(`${API_URL}/login/verify`, {
+  //   headers: {
+  //     Authorization: `Bearer ${token}`,
+  //   },
+  // });
+  // if (user.data) {
+  //   return true;
+  // }
+  // return false;
+  try {
+    const token = getCookie('token');
+    if (!token) {
+      return false;
+    }
+    const user = await axios.get(`${API_URL}/login/verify`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+    return true;
+  } catch {
+    return false;
+  }
+}
+
+export const deletePost = async (id) => {
+  await axios.delete(`${API_URL}/posts/${id}`, {
+    headers: {
+      Authorization: `Bearer ${getCookie('token')}`,
+    },
+  });
 }
